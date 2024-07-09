8274a5a47d8f01ed48cc6a71947be184e36e10c4
U-dias <nomd_adniss@yahoo.co.jp>
2024-07-10 08:04:24 +0900

    20240710

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -13,7 +13,7 @@ class RoomsController < ApplicationController
   end
 
   def create
-    @room = current_user.rooms.build
+    @room = current_user.rooms.build(room_params)
     if @room.save
       redirect_to listing_room_path(@room), notice: "保存完了"
     else
