4757b40aaa27070a1537d4e0de44d0f0d23344c0
U-dias <nomd_adniss@yahoo.co.jp>
2024-07-16 06:15:27 +0900

    240716

--- app/models/room.rb
+++ app/models/room.rb
@@ -6,7 +6,7 @@ class Room < ApplicationRecord
   has_many :guest_reviews
   has_many :reviews
 
-  validates :r_name, :r_self, :r_price, :r_create, :r_address, presence: true
+  validates :r_name, :r_self, :r_price, :r_create, :r_address, presence: true, allow_blank: true
   validates :r_price, numericality: {only_integer: true, greater_than_or_equal_to: 1}
 
   def average_rating
